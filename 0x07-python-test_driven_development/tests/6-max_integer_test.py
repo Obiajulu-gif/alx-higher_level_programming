bf8b6dbf0df3c2b92665ea0a67ad2b27217c3f6f
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-01-08 10:34:09 +0100

    task 6 done

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """Class for unittests
+    """
+    def test_max(self):
+        """Test for max
+        """
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([-4, -3, -2, -1]), -1)
+        self.assertEqual(max_integer([1, 2, 3, 4, float('inf')]), float('inf'))
+        self.assertEqual(max_integer([1, 2, 3, 4, float('-inf')]), 4)
+        self.assertEqual(max_integer([1, 2, 3, 4, float('nan')]), 4)
+        self.assertEqual(max_integer([1, 2, 3, 4, 4]), 4)
+        self.assertEqual(max_integer([1, 2, 3, 4, 4.0]), 4)
