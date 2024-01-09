6707070169bffb7ff708cb597deae811062090f6
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-01-09 13:05:01 +0100

    task four done

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""check if an object is an instance of a class that inherited
+ (directly or indirectly) from the specified class """
+
+
+def inherits_from(obj, a_class):
+    """
+    Checking for an inherited object instance
+
+    Args:
+    - obj: object to be checked
+    - a_class: class to be checked
+
+    Return:
+    - Return True if found or False if not found
+    """
+
+    return isinstance(obj, a_class) or issubclass(type(obj), a_class)
