76e1cb7b84f1f9b20a745c5e7b37eb9380cc9ea3
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-01-09 13:09:54 +0100

    task four done

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,4 +15,4 @@ def inherits_from(obj, a_class):
     - Return True if found or False if not found
     """
 
-    return isinstance(obj, a_class) and issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
