2ba2557033e731bfa2679fc9fda2224babf36bb3
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-01-10 00:37:43 +0100

    task one done

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function to read text file"""
+
+def read_file(filename=""):
+    """function that will read a .txt file
+
+    Args:
+    - filename: the name of the file
+
+    Return:
+    - Return all the text to the stdout
+    """
+
+    with open(filename, "r", encoding="UTF8") as filedoc:
+        fp = filedoc.read()
+        print(fp)
