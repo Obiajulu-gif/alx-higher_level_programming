db8ddb8fb95f4ff9ddf24912731b12a511e5105e
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-01-10 05:52:43 +0100

    task 7 done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-"""Module that adds all arguments to a Python list,
-and then save them to a file
-"""
+"""mdoule adds arguments to a python list"""
 
 
-import json
 import sys
-from os import path
-
-
-def save_to_json_file(my_obj, filename):
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
-
-
-def load_from_json_file(filename):
-    with open(filename, 'r') as file:
-        return json.load(file)
-
-
-if __name__ == "__main__":
-    file_name = "add_item.json"
-    data = []
-
-    if path.exists(file_name):
-        data = load_from_json_file(file_name)
-
-    data.extend(sys.argv[1:])
-    save_to_json_file(data, file_name)
+import os
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+filename = "add_item.json"
+try:
+    my_list = load_from_json_file(filename)
+except:
+    my_list = []
+if len(sys.argv) > 1:
+    for i in sys.argv[1:]:
+        my_list.append(i)
+save_to_json_file(my_list, filename)
