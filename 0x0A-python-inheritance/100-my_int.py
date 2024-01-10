76816a97d37a3a171148310b240e5520ef1d2039
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-01-11 00:53:18 +0100

    task 100 done

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""class MyInt that inherits from int"""
+
+
+class MyInt:
+    """inherit the int class from the in-built int class"""
+    def __init__(self, value):
+        self.__value = value
+
+    def __eq__(self, other):
+        if self.__value == other:
+            return False
+        else:
+            return True
+
+    def __ne__(self, other):
+        if self.__value != other:
+            return False
+        else:
+            return True
+
+    def __str__(self):
+        return "{}".format(self.__value)
