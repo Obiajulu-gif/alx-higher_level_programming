c99ee899a8dc18de19bcca6c6bcf964b949b5e03
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-01-14 23:52:36 +0100

    task 6 done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -65,11 +65,13 @@ class Rectangle(Base):
 
     def display(self):
         """Prints the rectangle instance with '#' character"""
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print('#' * self.__width)
+            print(' ' * self.__x + '#' * self.__width)
 
     def __str__(self):
         """String representation of the object"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                     self.x, self.y,
-                                                     self.width, self.height)
+                                                       self.x, self.y,
+                                                       self.width, self.height)
