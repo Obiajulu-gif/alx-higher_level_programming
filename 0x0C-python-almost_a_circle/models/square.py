0278e259ff1658c2f5b8db2d248b08ba0786f8d6
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-01-15 17:48:39 +0100

    task 10 done

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,9 +9,19 @@ class Square(Rectangle):
     """class Square that inherit from a Rectangle class"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.size = size
+        self.__size = size
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x, self.y,
                                                  self.size)
+    @property
+    def size(self):
+        """Getter for the size attribute"""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """Setter for the size attribute"""
+        self.width = value
+        self.height = value
