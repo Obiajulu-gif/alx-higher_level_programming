4ca8b0652595f4c714457e66091ea2470bb2f893
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-01-16 00:30:27 +0100

    task 14 done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """module for base class"""
 
+import json
+
 
 class Base:
     """class of a base"""
@@ -12,3 +14,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
