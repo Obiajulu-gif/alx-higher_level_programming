babeddf495d95095346069cb666d8e20122b14b4
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-02-16 20:29:42 +0100

    task 10 to 11

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- script that list all records of a table in an order
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- calculate the average of all records
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
