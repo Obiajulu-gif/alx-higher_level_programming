144d66d1c941b9ed2488460b73f6d909d5d919d5
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-03-13 02:52:48 +0100

    task 2 done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const args = process.argv;
-if (args.length === 2) {
-  console.log('No argument');
-} else if (args.length === 3) {
-  console.log('Argument found');
+const argv = process.argv;
+if (argv[2] === undefined) {
+	console.log("No argument");
 } else {
-  console.log('Arguments found');
+	console.log(argv[2]);
 }
