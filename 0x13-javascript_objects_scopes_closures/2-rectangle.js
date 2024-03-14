32e25b2c0f69e628b467ab5a6ebb4173e995fcb9
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-03-14 23:33:24 +0100

    task 2 done

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-
 class Rectangle {
   constructor (w, h) {
-    if (isNaN(w) || w <= 0 || isNaN(h) || h <= 0) {
-      this.width = undefined;
-      this.height = undefined;
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
