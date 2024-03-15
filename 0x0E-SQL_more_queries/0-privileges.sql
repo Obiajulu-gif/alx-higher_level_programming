c8d5370b24ecfc937e76292d8ad99915c1b24bbb
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-03-15 18:43:01 +0100

    task 0 done

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
+-- script that lists all the privilegde of mySql user
 echo "CREATE USER 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
 echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
 echo "CREATE USER 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
