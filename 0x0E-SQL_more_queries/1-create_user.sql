2d63f906cd862d8e9a193ee495e2b3b864aea1f2
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-03-15 19:00:52 +0100

    task 1 done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Script that create the MySql server user 
-echo "CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';" | mysql -hlocalhost -uroot -p
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
-echo "FLUSH PRIVILEGES;" | mysql -hlocalhost -uroot -p
+-- create a user user_0d_1
+-- with root privileges
+CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
