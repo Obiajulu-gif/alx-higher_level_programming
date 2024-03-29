5d9ddb8859a1c4dc7d797e2523ed8001c7f97906
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2024-03-29 16:05:11 +0100

    Delete 0x12-javascript-warm_up directory
    delete Javascript warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const arg = process.argv;
-if (Number.isInteger(parseInt(arg[2]))) {
-  console.log('My number: ' + arg[2]);
-} else {
-  console.log('Not a number');
-}
--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const intArray = process.argv;
-if (intArray < 3) {
-  console.log(NaN);
-} else {
-  const result = parseInt(intArray[2]) + parseInt(intArray[3]);
-  console.log(result);
-}
