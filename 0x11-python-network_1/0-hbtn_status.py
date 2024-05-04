e1279b6a22e9154702e07814c0a41b07054786ff
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-05-04 17:25:08 +0100

    task 0 done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    print("Body response:")
-    print("    - type:", type(body))
-    print("    - content:", body)
-    print("    - utf8 content:", body.decode('utf-8'))
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("    - type:", type(body))
+        print("    - content:", body)
+        print("    - utf8 content:", body.decode('utf-8'))
