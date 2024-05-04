eaff9928bf81328f8d9f51eae615e3697d317984
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-05-04 17:20:18 +0100

    task 0 done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
-
 import urllib.request
+
 url = 'https://alx-intranet.hbtn.io/status'
 
 with urllib.request.urlopen(url) as response:
-    html = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+    body = response.read()
+    print("Body response:")
+    print("    - type:", type(body))
+    print("    - content:", body)
+    print("    - utf8 content:", body.decode('utf-8'))
