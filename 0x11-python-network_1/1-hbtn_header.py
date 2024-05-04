16b7aaeeb05c5241b78f65b1bb5b6aa49ce694ca
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-05-04 17:53:24 +0100

    task 0 done

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.getheader('X-Request-Id'))
