e423254dbfc15c3f2156eb72bdc9a62bf8f38344
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-05-05 01:22:06 +0100

    task 7 done

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,7 +2,7 @@
 import requests
 import sys
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     url = sys.argv[1]
     response = requests.get(url)
     if response.status_code >= 400:
