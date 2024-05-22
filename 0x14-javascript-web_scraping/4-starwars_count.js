b1e39441761b3f1742924d7916ac9d7a8272a1a3
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-05-22 04:59:26 +0100

    task 4 done

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,17 +4,33 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 if (!apiUrl) {
-    console.error('Please provide an API URL');
-    process.exit(1);
+  console.error('Usage: ./4-starwars_count.js <API URL>');
+  process.exit(1);
 }
 
 const characterId = '18';
 
 request(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else {
-        const count = JSON.parse(body).count;
-        console.log(count);
+  if (error) {
+    console.error(error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error(`Failed to retrieve data: ${response.statusCode}`);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  let count = 0;
+
+  data.results.forEach((film) => {
+    if (
+      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`
+      )
+    ) {
+      count++;
     }
-})
+  });
+
+  console.log(count);
+});
