2c7cca7636fa1ce3f5f2bd9d8dce157404583b18
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-05-22 05:14:46 +0100

    task 4 done

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,36 +1,24 @@
 #!/usr/bin/node
 
-const request = require('request');
-const apiUrl = process.argv[2];
+const request = require("request");
+const url = process.argv[2];
 
-if (!apiUrl) {
-  console.error('Usage: ./4-starwars_count.js <API URL>');
-  process.exit(1);
-}
-
-const characterId = '18';
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  if (response.statusCode !== 200) {
-    console.error(`Failed to retrieve data: ${response.statusCode}`);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  let count = 0;
-
-  data.results.forEach((film) => {
-    if (
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`
-      )
-    ) {
-      count++;
-    }
-  });
-
-  console.log(count);
+request(url, function (err, response, body) {
+	if (err) {
+		console.log(err);
+	} else if (response.statusCode === 200) {
+		const films = JSON.parse(body).results;
+		let count = 0;
+		for (const filmIndex in films) {
+			const filmChars = films[filmIndex].characters;
+			for (const charIndex in filmChars) {
+				if (filmChars[charIndex].includes("18")) {
+					count++;
+				}
+			}
+		}
+		console.log(count);
+	} else {
+		console.log("An error occured. Status code: " + response.statusCode);
+	}
 });
